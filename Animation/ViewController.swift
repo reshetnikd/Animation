abee8ea6126503daf5a8d0ce4674b849a534d39e
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-24 22:46:43 +0300

    Add animate method with switch inside

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -22,6 +22,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(
+            withDuration: 1,
+            delay: 0,
+            options: [],
+            animations: {
+                switch self.currentAnimation {
+                case 0:
+                    break
+                    
+                default:
+                    break
+                }
+            },
+            completion: { finished in
+                sender.isHidden = false
+            }
+        )
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
